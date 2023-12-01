cac5eee013d8d5aa06c1c83c7e21ab77f2ccc9bd
HakaruN <cantwell.josh@protonmail.com>
2023-12-01 15:43:04 +0000

    nothing much

--- include/Triangle.h
+++ include/Triangle.h
@@ -15,7 +15,9 @@ typedef struct{
 } Triangle;
 
 Triangle triangleGen(Vertex* verts, Vec3 Pos, Texture* texture);
-//unsigned int triangleGenp(Triangle* pTriangle, Vertex* verts, Vec3 Pos, Texture* texture);
+
+//delete triangle
+//void triDeleteTriangle()
 void freeTriangle(Triangle* triangle);
 void barycentricCoords(Vec3 out, Vec3 vert0, Vec3 vert1, Vec3 vert2, Vec3 point);
 int triangleIntersect(Vec3 v0, Vec3 v1, Vec3 v2, Ray* ray, Vec3 intersectionPoint);
